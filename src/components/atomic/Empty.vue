e383f2c1f1ef24c3fcdd5b5f3d7fe167b4da9010
Emiliano <emii.bayona22@gmail.com>
2026-03-21 16:08:06 -0300

    Empty update

--- src/components/atomic/Empty.vue
+++ src/components/atomic/Empty.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full flex flex-col justify-center text-center items-center font-bold text-xl mt-20">
+    <div class="w-full flex flex-col justify-center text-center items-center font-bold text-xl mt-20 mb-5 px-5">
         <span>
             {{ title }}
         </span>
